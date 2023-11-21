2651beaf9eaf022ab7ee09c080f07650063702ed
zzAdam <amedragh@gmail.com>
2023-11-21 17:31:19 +0100

    init maven

---
+++ src/main/java/com/compolo/compolo/CompoloApplication.java
@@ -0,0 +1,13 @@
+package com.compolo.compolo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CompoloApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CompoloApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/compolo/compolo/CompoloApplicationTests.java
@@ -0,0 +1,13 @@
+package com.compolo.compolo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CompoloApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
