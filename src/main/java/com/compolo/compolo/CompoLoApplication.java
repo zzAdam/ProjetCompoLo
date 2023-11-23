23c835a403f53674378b40966d832cb80ccf9cc3
zzAdam <amedragh@gmail.com>
2023-11-23 15:13:10 +0100

    Init Project

---
+++ src/main/java/com/compolo/compolo/CompoLoApplication.java
@@ -0,0 +1,13 @@
+package com.compolo.compolo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CompoLoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CompoLoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/compolo/compolo/CompoLoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.compolo.compolo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CompoLoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
