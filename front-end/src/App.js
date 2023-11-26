7340c6cce9722f893531d8bf8ab3c2505ba3d175
zzAdam <amedragh@gmail.com>
2023-11-26 17:20:57 +0100

    adding routes

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,24 +1,32 @@
-import logo from './logo.svg';
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import HomePage from './components/HomePage';
+import BookListPage from './components/BookListPage';
+import BookDetailsPage from './components/BookDetailsPage';
+import CartPage from './components/CartPage';
+import LoginPage from './components/LoginPage';
+import RegisterPage from './components/RegisterPage';
+import UserOrdersPage from './components/UserOrdersPage';
+import LibrarianDashboardPage from './components/LibrarianDashboardPage';
+import AdminDashboardPage from './components/AdminDashboardPage';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="App">
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/books" element={<BookListPage />} />
+          <Route path="/books/:id" element={<BookDetailsPage />} />
+          <Route path="/cart" element={<CartPage />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/register" element={<RegisterPage />} />
+          <Route path="/my-orders" element={<UserOrdersPage />} />
+          <Route path="/librarian-dashboard" element={<LibrarianDashboardPage />} />
+          <Route path="/admin-dashboard" element={<AdminDashboardPage />} />
+          {/* Ajoutez d'autres routes au besoin */}
+        </Routes>
+      </div>
   );
 }
 
